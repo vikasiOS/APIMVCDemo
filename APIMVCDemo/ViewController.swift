15978a5e7dd2786a865d4c68af6b97117edd5210
theta-5-vikas <theta-5-vikas@wemos-mac-mini-vikas.local>
2018-04-06 18:02:23 +0530

    Initial Commit

---
+++ APIMVCDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  APIMVCDemo
+//
+//  Created by theta-5-vikas on 06/04/18.
+//  Copyright © 2018 Theta Technolabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
